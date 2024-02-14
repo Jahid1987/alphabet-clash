3acf3fad521abb3308af912d34da6ae2e9667b6a
Jahid Anwer <jahid.englaa@gmail.com>
2024-02-14 07:52:08 +0600

    play ground, score section added

---
+++ index.js
@@ -0,0 +1,7 @@
+const home = document.getElementById("home");
+const playGround = document.getElementById("play-ground");
+
+function play() {
+  home.classList.add("hidden");
+  playGround.classList.remove("hidden");
+}
